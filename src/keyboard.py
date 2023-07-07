d001c54526f869f6e3beebe972f4e9208b75d3fe
miroshnikoff <taxi83080@gmail.com>
2023-07-07 22:04:30 +0300

    add docstrings

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,15 +2,17 @@ from src.item import Item
 
 
 class MixinChangeLang:
-
+    """Расширяет функционал класса Keyboard. Добавляет язык и возможность его изменения"""
     def __init__(self):
         self.__language = "EN"
 
     @property
     def language(self):
+        """Возвращает язык клавиатуры"""
         return self.__language
 
     def change_lang(self):
+        """Изменяет язык клавиатуры. По умолчанию 'EN'"""
         if self.__language == "EN":
             self.__language = "RU"
         else:
@@ -19,6 +21,6 @@ class MixinChangeLang:
 
 
 class Keyboard(Item, MixinChangeLang):
-
+    """Класс для клавиатур, дочерний от Item"""
     def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
